cc82251ce5e05d0f9525c1b319c999279af12504
kariluvr <akiajoshua87@gmail.com>
2026-05-13 18:10:46 +0800

    added sms on server

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 require("dotenv").config({
     quiet:true
 });
-
+const fetch =
+    require("node-fetch");
+    
 const express =
     require("express");
 
@@ -90,7 +92,80 @@ app.use(
     "/api/weather-risk",
     require("./server/routes/weatherRiskRoutes")
 );
+/*
+    SEND SMS
+*/
+app.post(
+    "/api/send-sms",
+    async(req,res)=>{
 
+        try{
+
+            const {
+                number,
+                message
+            } = req.body;
+
+            const response =
+                await fetch(
+
+                    "https://www.iprogsms.com/api/v1/sms_messages",
+
+                    {
+
+                        method:"POST",
+
+                        headers:{
+
+                            "Content-Type":
+                            "application/json"
+
+                        },
+
+                        body:JSON.stringify({
+
+                            api_token:
+                                process.env.IPROGSMS_API_TOKEN,
+
+                            phone_number:
+                                number,
+
+                            message:
+                                message
+
+                        })
+
+                    }
+
+                );
+
+            const data =
+                await response.json();
+
+            console.log(data);
+
+            res.json({
+
+                success:true,
+                data
+
+            });
+
+        }catch(error){
+
+            console.log(error);
+
+            res.status(500).json({
+
+                success:false,
+                error:error.message
+
+            });
+
+        }
+
+    }
+);
 /*
     LOGIN API
 */

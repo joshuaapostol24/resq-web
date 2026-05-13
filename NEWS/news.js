01413ed7251308cbce6c65ae23aedc969d29def3
kariluvr <akiajoshua87@gmail.com>
2026-05-13 20:35:14 +0800

    try news sms api

--- NEWS/news.js
+++ NEWS/news.js
@@ -326,7 +326,7 @@ form.addEventListener(
             console.log(result);
 
             await fetch(
-                "https://resq-app-xsb98.ondigitalocean.app/sms/test",
+                "https://resq-app-xsb98.ondigitalocean.app/send-sms",
                 {
 
                     method:"POST",
